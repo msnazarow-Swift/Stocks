019782445fa20e6e091e0ca55b0f111cfed11599
Mihail Nazaro <msnazarow@gmail.com>
2021-09-05 22:37:43 +0300

    feat(Initial): Project Started

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by out-nazarov2-ms on 05.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
